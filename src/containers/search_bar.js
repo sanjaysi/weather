1669d66cfec3a726c3c209b78b667c2892e431f6
unknown <Sanjay.Singh@ATMECSINLT-011.ATMECS.CORP>
2016-08-21 01:36:00 +0530

    52 Controlled Components and Binding Context

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({term: event.target.value});
+	}
+
 	render() {
 		return(
 			<form className="input-group">
-				<input />
+				<input 
+					placeholder="Enter a city name"
+					className="form-control"
+					value={this.state.term}
+					//onChange={(event) => this.onInputChange(event)}
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
-					<button type="submit" className="btn btn-secondary">Search</button>	
+					<button type="submit" className="btn btn-secondary">Submit</button>	
 				</span>
 			</form>
 		);
